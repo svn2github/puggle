d7063e7cfbed7b5b07edf5df8ef7c93316394048
gvasil <gvasil@3e796c23-a241-0410-b47b-d01935ffd6c6>
2007-12-20 19:07:39 +0000

    puggle-indexer.bat script fixed.
    git-svn-id: svn://svn.code.sf.net/p/puggle/code@7 3e796c23-a241-0410-b47b-d01935ffd6c6

--- bin/puggle-indexer.bat
+++ bin/puggle-indexer.bat
@@ -1,2 +1,21 @@
-java -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel -Xms2M -Xmx512M -cp ..\dist\Puggle.jar puggle.ui.IndexerFrame
+REM java -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel -Xms2M -Xmx512M -cp ..\dist\Puggle.jar puggle.ui.IndexerFrame
+
+@echo off
+
+REM java -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel -Xms2M -Xmx512M -jar "..\dist\Puggle.jar"
+
+REM ------------------------
+REM -- Check for debug run
+REM ------------------------
+SET JAVA_BIN=javaw
+CALL "%BIN%\debug.bat" %*
+IF "%DEBUG"=="YES" SET JAVA_BIN=java
+
+
+REM ------------------------
+REM -- Run Puggle
+REM ------------------------
+START %JAVA_BIN%  -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel -Xms2M -Xmx512M -cp ..\dist\Puggle.jar puggle.ui.IndexerFrame  %*
+
+if "Windows_NT"=="%OS%" endlocal
 
