f1b28af2d69c0c06966efb2c77e126df2d174592
gvasil <gvasil@3e796c23-a241-0410-b47b-d01935ffd6c6>
2009-12-27 15:41:16 +0000

    corrected unix shell scripts and added a new one for puggle portable portable
    git-svn-id: svn://svn.code.sf.net/p/puggle/code@73 3e796c23-a241-0410-b47b-d01935ffd6c6

--- bin/puggle-indexer.sh
+++ bin/puggle-indexer.sh
@@ -1,11 +1,6 @@
-<<<<<<< .mine
-#!/bin/bash
-java -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel -Xms2M -Xmx512M -cp ..\dist\Puggle.jar puggle.ui.IndexerFrame
-=======
 #!/bin/bash
 
 export JAVA_BIN=java
 
 $JAVA_BIN -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel -Xms2M -Xmx512M -cp ../dist/Puggle.jar puggle.ui.IndexerFrame  $*
 
->>>>>>> .r66
--- bin/puggle.sh
+++ bin/puggle.sh
@@ -1,13 +1,6 @@
-<<<<<<< .mine
-#!/bin/bash
-
-java -Xms2M -Xmx512M -cp ..\dist\Puggle.jar -jar "..\dist\Puggle.jar"
-
-=======
 #!/bin/bash
 
 export JAVA_BIN=java
 
 $JAVA_BIN -Xms2M -Xmx512M -cp ../dist/Puggle.jar -jar "../dist/Puggle.jar" $*
 
->>>>>>> .r66
